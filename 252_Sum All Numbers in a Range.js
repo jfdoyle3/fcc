eb912de3c72ab977cea4562bd2534e8d3e20e115
Jim Doyle <indellibleink@gmail.com>
2018-04-09 19:48:32 -0400

    Completed

--- 252_Sum All Numbers in a Range.js
+++ 252_Sum All Numbers in a Range.js
@@ -1,15 +1,16 @@
+
 function sumAll(arr) {
- //  arr.sort(function(a,b) {
- //    return a-b;
- //});
+  arr.sort(function(a,b) {
+    return a-b;
+ });
       var min=Math.min(arr[0]);
-      var max=Math.max(arr.length);
-
-  for (i=0; i < max;i++){
-    console.log(i);
-
-};
-   return arr;
+      var max=Math.max(arr[arr.length-1]);
+var total=0;
+  for (i=min; i <=max; i++){
+    
+ total += i; 
+}
+   return total;
  }
-//console.log(
-sumAll([1, 4]);
+ //print outfunction to console
+console.log(sumAll([10, 5]));
