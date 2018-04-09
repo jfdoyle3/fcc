66104efcabc14a87decbb587768a84998d66adb2
Jim Doyle <indellibleink@gmail.com>
2018-04-09 00:58:04 -0400

    Add: Sum All Numbers in a Range

---
+++ 252_Sum All Numbers in a Range.js
@@ -0,0 +1,15 @@
+function sumAll(arr) {
+ //  arr.sort(function(a,b) {
+ //    return a-b;
+ //});
+      var min=Math.min(arr[0]);
+      var max=Math.max(arr.length);
+
+  for (i=0; i < max;i++){
+    console.log(i);
+
+};
+   return arr;
+ }
+//console.log(
+sumAll([1, 4]);
