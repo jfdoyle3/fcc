09d54eac0140d5c35647cce32882bd57ddd666c9
Jim Doyle <indellibleink@gmail.com>
2018-04-09 20:42:41 -0400

    Setup Problem and add for loops

---
+++ 253_Diff Two Arrays.js
@@ -0,0 +1,14 @@
+function diffArray(arr1, arr2) {
+    var newArr = [];
+    console.log(newArr);
+    for(i=0; ;i++){
+
+        for (j=0; ;j++){
+
+        }
+    }
+    // Same, same; but different.
+    return newArr;
+  }
+  
+  diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
