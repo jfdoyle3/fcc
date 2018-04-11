10c75eeedd85d9f33db3d062be511388015d6046
Jim Doyle <indellibleink@gmail.com>
2018-04-10 22:04:55 -0400

    Completed: Diff Two Arrays

--- 253_Diff Two Arrays.js
+++ 253_Diff Two Arrays.js
@@ -1,14 +1,24 @@
-function diffArray(arr1, arr2) {
-    var newArr = [];
-    console.log(newArr);
-    for(i=0; ;i++){
 
-        for (j=0; ;j++){
-
-        }
-    }
-    // Same, same; but different.
-    return newArr;
-  }
+function diffArray(arr1, arr2) { 
+    for(i = 0; i < arr1.length; i++)
+      {
+          for(j = 0; j < arr2.length; j++)
+          {
+              if(arr1[i] == arr2[j])
+              {
+                  arr1.splice(i, 1);
+                  arr2.splice(j, 1);
+                  i--;
+                  j--;
   
-  diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+              }
+  
+          }
+  
+      }
+  
+    // Same, same; but different.
+    return arr1.concat(arr2);
+  }
+//print out function to console
+ console.log(diffArray([10, 2, 3, 5], [1, 2, 3, 4, 5]);
