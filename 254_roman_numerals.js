edafe40e9b67ab4554f51693f22f053045b0f56e
Jim Doyle <indellibleink@gmail.com>
2018-04-11 22:15:21 -0400

    Updated file

---
+++ 254_roman_numerals.js
@@ -0,0 +1,17 @@
+
+function convertToRoman(num) {
+	var rNum=["I","V"];
+	var dNum=[1,5];
+	 rom_num=[];
+	for (i=0; i< num; i++){
+	  if (num<=3){
+	 pNum=
+	  pNum.push(rom_num);
+  	}                       
+	}
+console.log(rNum);
+ return rom_num;
+}
+
+//console.log(convertToRoman(3));
+convertToRoman(3);
