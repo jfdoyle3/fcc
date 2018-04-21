33d4b67e258de44cbc05399ea831daa035190491
Jim Doyle <indellibleink@gmail.com>
2018-04-21 10:34:40 -0400

    Working through probem

--- 255_Wherefore_art_thou.js
+++ 255_Wherefore_art_thou.js
@@ -1,12 +1,16 @@
 function whatIsInAName(collection, source) {
     // What's in a name?
+ console.log(collection);
     var arr = [];
     // Only change code below this line
-    
+// for ( in collection)
+
     
     // Only change code above this line
     return arr;
+
   }
+
   
   whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
   
