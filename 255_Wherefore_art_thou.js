ed364b05b44964457eb966fddae830ccf3ebd488
Jim Doyle <indellibleink@gmail.com>
2018-04-24 18:08:02 -0400

    removed unfinish line

--- 255_Wherefore_art_thou.js
+++ 255_Wherefore_art_thou.js
@@ -5,10 +5,10 @@ function whatIsInAName(collection, source) {
     // Only change code below this line
 for (var i in collection){
     console.log(collection[i]);
-  //  string1 = string1 + arr[name];
+
 console.log('Object.keys(collection[i]) = ' + Object.keys(collection[i]));
   for (var j in collection[j]);
-    if  collection[i] == 
+
 
 }
     // Only change code above this line
