1e9413d772201130144c7b1b6aebfd9daffd886e
Jim Doyle <indellibleink@gmail.com>
2018-04-23 20:07:20 -0400

    completed

--- Telephone_numbers.js
+++ Telephone_numbers.js
@@ -1,8 +1,9 @@
 function telephoneCheck(str) {
   // Good luck!
-  return true;
-}
+  var number = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+  return number.test(str);
 
+}
 
 
 telephoneCheck("555-555-5555");
