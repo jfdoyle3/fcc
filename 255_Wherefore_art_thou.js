a518bb18cc8e3df68d56169233ee5d4a7e673d95
Jim Doyle <indellibleink@gmail.com>
2018-04-28 09:57:32 -0400

    working on alogrithm

--- 255_Wherefore_art_thou.js
+++ 255_Wherefore_art_thou.js
@@ -4,14 +4,16 @@ function whatIsInAName(collection, source) {
     var arr = [];
     // Only change code below this line
 for (var i in collection){
-    console.log(collection[i]);
- console.log (collection[i]["c"]);
-console.log('Object.keys(collection[i]) = ' + Object.keys(collection[i]));
-console.log(collection[i].hasOwnProperty("c"));
-
+//    console.log(collection[i]);
+// console.log (collection[i]["c"]);
+// console.log('Object.keys(collection[i]) = ' + Object.keys(collection[i]));
+// console.log(collection[i].hasOwnProperty("c"));
+//console.log(i + collection[i].hasOwnProperty(Object.keys(collection[i])));
 //  for (var j in collection[j]);
-
-
+//console.log(Object.entries(collection)[i]);
+    if (Object.hasOwnProperty(i)){
+        console.log(i);
+    }
 }
     // Only change code above this line
     return arr;
