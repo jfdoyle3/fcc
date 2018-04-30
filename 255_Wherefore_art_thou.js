fbfa82b2c0a39fd7b4bfac7ad6139c929e59d354
Jim Doyle <indellibleink@gmail.com>
2018-04-30 18:26:25 -0400

    Updated Code

--- 255_Wherefore_art_thou.js
+++ 255_Wherefore_art_thou.js
@@ -3,30 +3,20 @@ function whatIsInAName(collection, source) {
     
     var arr = [];
     // Only change code below this line
-//for (var i in collection){
-// if (source[Object.keys[source]]==collection[Object.keys(collection[i]){
-//     collection.push(arr);
- //    console.log(arr);
-// }
-
+for (i in collection){
 
     //COLLECTION
-       console.log(collection[i]);
- console.log (collection[i]["last"]);
- console.log('Object.keys(collection[i]) = ' + Object.keys(collection[i]));
- console.log(collection[i].hasOwnProperty("c"));
- console.log(i+ " " + collection[i].hasOwnProperty(Object.keys(collection[i])));
+ //console.log(collection[i]);
+ //console.log (collection[i]["last"]);
+ //console.log('Object.keys(collection[i]) = ' + Object.keys(collection[i]));
+ //console.log(collection[i].hasOwnProperty("c"));
+ //console.log(i+ " " + collection[i].hasOwnProperty(Object.keys(collection[i])));
  //SOURCE
 //console.log(source);
 //console.log(Object.keys(source));
 //console.log(source.hasOwnProperty(source[i]));
 console.log(source[Object.keys(source)]);
-//  for (var j in collection[j]);
-//console.log(Object.entries(collection)[i]);
- //   if (Object.hasOwnProperty(i)){
-       // console.log(i);
-    }
-
+}
     // Only change code above this line
     return arr;
 }
---
+++ 256_Search_&_Replace.js
@@ -0,0 +1,33 @@
+function myReplace(str, before, after) {
+// var n=str.replace(before, after);
+// console.log(after[0]);
+var b=before.charCodeAt(0);
+var a=after.charCodeAt(0);
+console.log(a);
+if (b < a){
+	 after= a-b;
+} else 
+ if(a>b){
+ 	after=a +b
+ }
+//if (after == "john"){
+//   var after='John';
+ //} else 
+// if (after == "sitting"){
+// 	var after="Sitting";
+// } else 
+ //if(after == "algorithms"){
+// 	var after="Algorithms";
+// }
+ 
+ //console.log(after);
+var n=str.replace(before,after);	
+  
+ 
+ console.log(n);
+  return n;
+}
+myReplace("He is Sleeping on the couch", "Sleeping", "sitting")
+//myReplace("Let us get back to more Coding", "Coding", "algorithms")
+//myReplace("His name is Tom", "Tom", "john");
+// myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
