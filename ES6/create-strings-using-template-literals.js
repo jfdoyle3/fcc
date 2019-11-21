4e1a4435359d832d45a29d157d901f621dbda8d2
Jim Doyle <indellibleink@gmail.com>
2019-11-20 21:57:01 -0500

    Completed more ES6 excerises

---
+++ ES6/create-strings-using-template-literals.js
@@ -0,0 +1,25 @@
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["id-blacklist", "no-dup-keys"]
+};
+function makeList(arr) {
+  "use strict";
+
+  // change code below this line
+  let resultDisplayArray =[];
+  for (let idx=0; idx<result.failure.length; idx++)
+  {
+   resultDisplayArray.push(`<li class="text-warning">${result.failure[idx]}</li>`);
+  } 
+  // change code above this line
+
+  return resultDisplayArray;
+}
+/**
+ * makeList(result.failure) should return:
+ * [ `<li class="text-warning">no-var</li>`,
+ *   `<li class="text-warning">var-on-top</li>`,
+ *   `<li class="text-warning">linebreak</li>` ]
+ **/
+const resultDisplayArray = makeList(result.failure);
---
+++ ES6/use-class-syntax-to-define-a-constructor-function.js
@@ -0,0 +1,10 @@
+/* Alter code below this line */
+class Vegetable{
+    constructor(vegetable){
+        this.name=vegetable;
+    }
+}
+/* Alter code above this line */
+
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // => should be 'carrot'
