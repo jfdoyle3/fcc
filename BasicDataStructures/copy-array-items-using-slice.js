1795877815ecb8f0b1a898cc3370e2e7d8a2dd6a
Jim Doyle <indellibleink@gmail.com>
2019-12-01 12:12:32 -0500

    Completed 4 Basic Data Structure challenges

---
+++ BasicDataStructures/copy-array-items-using-slice.js
@@ -0,0 +1,8 @@
+function forecast(arr) {
+  // change code below this line
+let weather=arr.slice(2,4);
+  return weather;
+}
+
+// do not change code below this line
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
